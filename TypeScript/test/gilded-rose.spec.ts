41468f0d8614f6eda778730e5b32f7189db74a79
Maxime Martin <maxime@ornikar.com>
2021-04-21 16:15:44 +0200

    test(gilded-rose): cover every-lines

--- TypeScript/test/gilded-rose.spec.ts
+++ TypeScript/test/gilded-rose.spec.ts
@@ -1,19 +1,7 @@
 import { expect } from 'chai';
 import { Item, GildedRose } from '../app/gilded-rose';
 
-const defaultItems = [
-    new Item("+5 Dexterity Vest", 10, 20), //
-    new Item("Aged Brie", 2, 0), //
-    new Item("Elixir of the Mongoose", 5, 7), //
-    new Item("Sulfuras, Hand of Ragnaros", 0, 80), //
-    new Item("Sulfuras, Hand of Ragnaros", -1, 80),
-    new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
-    new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
-    new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
-    // this conjured item does not work properly yet
-    new Item("Conjured Mana Cake", 3, 6)];
-
-describe('Gilded Rose – updateQuality', function () {
+describe('Gilded Rose', function () {
 
     describe('updateQuality', function() {
         describe('Classical products', function() {
